85a09b1676c0bf05bf645a7b28c50d03b1ed3087
jolfr <thomas.jack.carroll@gmail.com>
2024-12-25 17:04:31 -0500

    fix(refactor): refactored tests to use shared assert file function

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,19 +1,24 @@
 import os
+import pytest
+
+def assert_file(root_path, relative_path):
+    """
+    Asserts whether a file exists at a path
+
+    Args:
+        root_path (string): The absolute path to the folder which contains the file
+        relative_path (string): The relative path and name of the file
+    """
+    assert os.path.exists(f"{root_path}/{relative_path}"), f"{relative_path} does not exist"
 
 def test_project_structure(setup_and_teardown):
+    """
+    Test for the existence of various files expected to be in the project.
+    """
     project_path = setup_and_teardown["project_path"]
     
-    pyproject = f"{project_path}/pyproject.toml"
-    assert os.path.exists(pyproject), "pyproject.toml does not exist"
-    
-    readme = f"{project_path}/README.md"
-    assert os.path.exists(readme), "README.md does not exist"
-    
-    python_version = f"{project_path}/.python-version"
-    assert os.path.exists(python_version), ".python-version does not exist"
-    
-    gitignore = f"{project_path}/.gitignore"
-    assert os.path.exists(gitignore), ".gitignore does not exist"
-    
-    main = f"{project_path}/app/main.py"
-    assert os.path.exists(main), "app/main.py does not exist"
+    assert_file(project_path, "pyproject.toml")
+    assert_file(project_path, "README.md")
+    assert_file(project_path, ".python-version")
+    assert_file(project_path, ".gitignore")
+    assert_file(project_path, "app/main.py")
