e7eb43b3bc3043dee3b0e4cd22b132156adfbe97
jolfr <thomas.jack.carroll@gmail.com>
2025-01-11 14:29:24 -0500

    fix: updating hook error handling to print error stack

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,27 +4,27 @@ import subprocess
 def initialize_version_control() -> bool:
     print("Initializing version control...", end="")
     try:
-        subprocess.run(["mv", ".env.template", ".env"], capture_output=True, check=True)
-        subprocess.run(["git", "init"], capture_output=True, check=True)
-        subprocess.run(["git", "add", "."], capture_output=True, check=True)
-        subprocess.run(["git", "commit", "-m", "chore: initialized project from langchain-application-template"], capture_output=True, check=True)
+        subprocess.run(["mv", ".env.template", ".env"], capture_output=True, check=True, text=True,)
+        subprocess.run(["git", "init"], capture_output=True, check=True, text=True)
+        subprocess.run(["git", "add", "."], capture_output=True, check=True, text=True)
+        subprocess.run(["git", "commit", "-m", "chore: initialized project from langchain-application-template"], capture_output=True, check=True, text=True)
         print("done.")
         return True
-    except Exception as e:
-        print("")
-        print(e)
+    except subprocess.CalledProcessError as e:
+        print("\nCommand failed with error:")
+        print(e.stderr)
         return False
     
     
 def initialize_environment() -> bool:
     print("Initializing virtual environment...", end="")
     try:
-        subprocess.run(["uv", "sync"], capture_output=True, check=True)
+        subprocess.run(["uv", "sync"], capture_output=True, check=True, text=True)
         print("done.")
         return True
-    except Exception as e:
-        print("")
-        print(e)
+    except subprocess.CalledProcessError as e:
+        print("\nCommand failed with error:")
+        print(e.stderr)
         return False
 
 if __name__ == "__main__":
